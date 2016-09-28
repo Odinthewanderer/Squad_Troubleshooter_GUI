15d073bfa04bc883668038203f1985b2db62b0cf
Odinthewanderer <webster5361@gmail.com>
2016-09-28 17:50:48 -0500

    Initial commit

---
+++ Squad_Troubleshooter/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Squad_Troubleshooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nukeBtn = new System.Windows.Forms.Button();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.reinstallBtn = new System.Windows.Forms.Button();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.installBtn = new System.Windows.Forms.Button();
+            this.disableBtn = new System.Windows.Forms.Button();
+            this.enableBtn = new System.Windows.Forms.Button();
+            this.getHelpBtn = new System.Windows.Forms.Button();
+            this.output_textbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // nukeBtn
+            // 
+            this.nukeBtn.Location = new System.Drawing.Point(181, 66);
+            this.nukeBtn.Name = "nukeBtn";
+            this.nukeBtn.Size = new System.Drawing.Size(267, 23);
+            this.nukeBtn.TabIndex = 0;
+            this.nukeBtn.Text = "Nuke AppData Config Files";
+            this.nukeBtn.UseVisualStyleBackColor = true;
+            this.nukeBtn.Click += new System.EventHandler(this.nukeBtn_Click);
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(181, 95);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(267, 23);
+            this.generateBtn.TabIndex = 1;
+            this.generateBtn.Text = "Generate Windows Event Log";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // reinstallBtn
+            // 
+            this.reinstallBtn.Location = new System.Drawing.Point(181, 124);
+            this.reinstallBtn.Name = "reinstallBtn";
+            this.reinstallBtn.Size = new System.Drawing.Size(267, 23);
+            this.reinstallBtn.TabIndex = 2;
+            this.reinstallBtn.Text = "Re-install Easy Anti-Cheat (EAC)";
+            this.reinstallBtn.UseVisualStyleBackColor = true;
+            this.reinstallBtn.Click += new System.EventHandler(this.reinstallBtn_Click);
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Location = new System.Drawing.Point(181, 153);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(267, 23);
+            this.copyBtn.TabIndex = 3;
+            this.copyBtn.Text = "Copy Squad Logs to Desktop";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // installBtn
+            // 
+            this.installBtn.Location = new System.Drawing.Point(181, 182);
+            this.installBtn.Name = "installBtn";
+            this.installBtn.Size = new System.Drawing.Size(267, 23);
+            this.installBtn.TabIndex = 4;
+            this.installBtn.Text = "Install VC Redistributable 2013 + 2015";
+            this.installBtn.UseVisualStyleBackColor = true;
+            this.installBtn.Click += new System.EventHandler(this.installBtn_Click);
+            // 
+            // disableBtn
+            // 
+            this.disableBtn.Location = new System.Drawing.Point(181, 211);
+            this.disableBtn.Name = "disableBtn";
+            this.disableBtn.Size = new System.Drawing.Size(267, 23);
+            this.disableBtn.TabIndex = 5;
+            this.disableBtn.Text = "Disable Windows Firewall";
+            this.disableBtn.UseVisualStyleBackColor = true;
+            this.disableBtn.Click += new System.EventHandler(this.disableBtn_Click);
+            // 
+            // enableBtn
+            // 
+            this.enableBtn.Location = new System.Drawing.Point(181, 240);
+            this.enableBtn.Name = "enableBtn";
+            this.enableBtn.Size = new System.Drawing.Size(267, 23);
+            this.enableBtn.TabIndex = 6;
+            this.enableBtn.Text = "Enable Windows Firewall";
+            this.enableBtn.UseVisualStyleBackColor = true;
+            this.enableBtn.Click += new System.EventHandler(this.enableBtn_Click);
+            // 
+            // getHelpBtn
+            // 
+            this.getHelpBtn.Location = new System.Drawing.Point(181, 269);
+            this.getHelpBtn.Name = "getHelpBtn";
+            this.getHelpBtn.Size = new System.Drawing.Size(267, 23);
+            this.getHelpBtn.TabIndex = 7;
+            this.getHelpBtn.Text = "Get help with game crashing on server browser";
+            this.getHelpBtn.UseVisualStyleBackColor = true;
+            this.getHelpBtn.Click += new System.EventHandler(this.getHelpBtn_Click);
+            // 
+            // output_textbox
+            // 
+            this.output_textbox.Location = new System.Drawing.Point(12, 323);
+            this.output_textbox.Name = "output_textbox";
+            this.output_textbox.ReadOnly = true;
+            this.output_textbox.Size = new System.Drawing.Size(583, 310);
+            this.output_textbox.TabIndex = 8;
+            this.output_textbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 637);
+            this.Controls.Add(this.output_textbox);
+            this.Controls.Add(this.getHelpBtn);
+            this.Controls.Add(this.enableBtn);
+            this.Controls.Add(this.disableBtn);
+            this.Controls.Add(this.installBtn);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.reinstallBtn);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.nukeBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SuicidalChair\'s Improved Squad Troubleshooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nukeBtn;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.Button reinstallBtn;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Button installBtn;
+        private System.Windows.Forms.Button disableBtn;
+        private System.Windows.Forms.Button enableBtn;
+        private System.Windows.Forms.Button getHelpBtn;
+        private System.Windows.Forms.RichTextBox output_textbox;
+    }
+}
+
---
+++ Squad_Troubleshooter/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Squad_Troubleshooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Deletes the squad folder in %LocalAppData% (this will wipe your video / control settings in game). 
+        // a good option if a patch has been released and something doesn't seem right.
+        private void nukeBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Nuke AppData config files\n");
+            output_textbox.AppendText("=========================================\n");
+            
+            try
+            {
+                var path = Environment.GetEnvironmentVariable("LocalAppData");
+                path = path + "\\Squad";
+
+                // DEBUG STATEMENT : REMOVE WHEN COMPLETED
+                //output_textbox.AppendText(path);
+
+                System.IO.Directory.Delete(path, true);
+                output_textbox.AppendText("AppData directory ( " + path + " ) has been successfully nuked!");
+            }
+            catch (IOException ie)
+            {
+                output_textbox.AppendText(ie.Message);
+            }
+        }
+
+        // Generates a Windows Event log, good to do after Squad Crashes to desktop and you can't figure out why, 
+        // it will place the log file on your desktop, upload it to somewhere on the internet and attach it on 
+        // the forums or email it to support@joinsquad.com for myself to take a look at it.
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Generate Windows event logs\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // Launches the EasyAntiCheat installer in the Squad install directory, make sure you pick squad as your game.
+        private void reinstallBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Reinstall Easy Anticheat (EAC)\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // Copies all squad log files including UE4 Dump files to your desktop into a SQUAD logs folder, zip it up 
+        // or send individual files.
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Copy Squad Logs to Desktop\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // Attempts to silently install VC Redistributables 2013 and 2015, if any error occurs you may have to 
+        // make sure your copy of windows is up to date, make sure no updates are available
+        private void installBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Install VC Redistributable 2013 + 2015\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // Disables Windows firewall, as a troubleshooting step if your having networking issues
+        private void disableBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Disable Windows Firewall\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // Enable Windows firewall, (for when F didn't solve your issue and you want to undo)
+        private void enableBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Enable Windows Firewall\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+
+        // will provide a URL to visit with the confirmed fix for server browser crashes.
+        private void getHelpBtn_Click(object sender, EventArgs e)
+        {
+            output_textbox.Clear();
+            output_textbox.AppendText("Get help with game crashing on Server Browser\n");
+            output_textbox.AppendText("=========================================\n");
+        }
+    }
+}
